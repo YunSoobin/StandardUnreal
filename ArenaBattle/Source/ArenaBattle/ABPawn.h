2b3e520007da8fca390777e88f93cf5b8e30800f
YunSoobin <soobin5296@gmail.com>
2024-01-30 13:48:26 +0900

    Chapter05 폰의 제작과 조작 - 폰의 구성 요소 - 폰의 조작 - 애니메이션의 설정

--- ArenaBattle/Source/ArenaBattle/ABPawn.h
+++ ArenaBattle/Source/ArenaBattle/ABPawn.h
@@ -4,7 +4,9 @@
 
 #include "ArenaBattle.h"
 #include "GameFramework/Pawn.h"
+#include "GameFramework/FloatingPawnMovement.h"
 #include "ABPawn.generated.h"
+
 
 UCLASS()
 class ARENABATTLE_API AABPawn : public APawn
@@ -28,5 +30,24 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(VisibleAnywhere, Category=Collision)
+	UCapsuleComponent* Capsule;
+
+	UPROPERTY(VisibleAnywhere, Category=Visual)
+	USkeletalMeshComponent* Mesh;
+
+	UPROPERTY(VisibleAnywhere, Category=Movement)
+	UFloatingPawnMovement* Movement;
+
+	UPROPERTY(VisibleAnywhere, Category=Camera)
+	USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere, Category=Camera)
+	UCameraComponent* Camera;
+
+private:
+	void UpDown(float NewAxisValue);
+	void LeftRight(float NewAxisValue);
 
 };
